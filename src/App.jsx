41b2b7642f663a1e1cc5a383eca13ab41d92c766
believer-15 <ujjwalsinha15@gmail.com>
2024-08-26 21:05:18 +0530

    Added presenter container design pattern

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import Home from "./Pages/Home";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home";
+import Signup from "./Pages/Auth/Signup";
+import Login from "./Pages/Auth/Login";
+import NotFound from "./Pages/NotFound";
+import Denied from "./Pages/Denied";
+import AddProduct from "./Pages/Admin/Addproduct";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/denied" element={<Denied />} />
+        <Route path="/auth/signup" element={<Signup />} />
+        <Route path="/auth/login" element={<Login />} />
+
+        <Route path="/admin/addProduct" element={<AddProduct />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
